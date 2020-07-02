1c58e12e062e65d093d1a461877767182b6b72b5
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-07-02 21:51:19 +0200

    created controller, routes and views for Teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,18 +1,42 @@
 class TeamsController < ApplicationController
 
   def index
+    # @teams = Teams.where
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(team_params)
+    @team.user_id = current_user.id
   end
 
   def update
+    set_team
+    @team.update(team_params)
+    redirect_to new_team_ingredient_path(@team)
   end
 
   def show
+    set_team
+    @user = User.find(@team.user_id)
+  end
+
+  def edit
+    set_team
+  end
+
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:team).permit(:members)
   end
 
 end
