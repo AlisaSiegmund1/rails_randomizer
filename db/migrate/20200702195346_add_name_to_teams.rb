97021fa12d61041e80900c7bf20964624fd03d1d
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-07-02 21:58:24 +0200

    added name to teams table

---
+++ db/migrate/20200702195346_add_name_to_teams.rb
@@ -0,0 +1,4 @@
+class AddNameToTeams < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_185502) do
+ActiveRecord::Schema.define(version: 2020_07_02_195346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
