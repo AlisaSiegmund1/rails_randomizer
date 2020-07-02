d8c2ffb221d24140527f89f2c48d6f7614ff56d7
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-07-02 21:30:44 +0200

    fixed animation in page header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,20 +17,26 @@
 
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml6 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var textWrapper = document.querySelector('.page-header .letters');
+textWrapper.addEventListener('mouseover', animation);
+// textWrapper.addEventListener('mouseout', animationfalse);
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 750,
-    delay: (el, i) => 50 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+function animation () {
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.page-header .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 10000,
+      delay: (el, i) => 50 * i
+    }).add({
+      targets: '.page-header',
+      opacity: 0,
+      duration: 10000,
+      easing: "easeOutExpo",
+      delay: 10000
+    });
+
+}
