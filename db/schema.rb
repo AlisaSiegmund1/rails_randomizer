e2fc24801f3cc97dd48140b191ae381518a26172
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-07-04 13:55:43 +0200

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_185502) do
+ActiveRecord::Schema.define(version: 2020_07_02_201137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "teams", force: :cascade do |t|
     t.bigint "user_id"
+    t.string "name"
     t.string "members", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
